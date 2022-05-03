31dad9f030916c7b6653fb2d7d654813d3d75fae
zhenekq <zhenek02ss@gmail.com>
2022-05-03 15:11:15 +0300

    reworked: created pagination, enter size on one page(very bad, by for this stage very good), input for searching by name

--- src/app/candidates/candidates.component.ts
+++ src/app/candidates/candidates.component.ts
@@ -13,6 +13,10 @@ export class CandidatesComponent implements OnInit {
   public candidates: Candidate[] = []
   id: string = "";
   status: string = "";
+  emails: string[] = []
+  pageNumber: number = 1;
+  pageSize: number = 2;
+
 
   constructor(private candidateService: CandidateService) {
   }
@@ -20,7 +24,7 @@ export class CandidatesComponent implements OnInit {
   public getCandidates(): void {
     this.candidateService.getCandidates().subscribe(
       (response: Candidate[]) => {
-        this.candidates = response;
+        this.candidates = response
       },
       (error: HttpErrorResponse) => {
         alert(error.message)
@@ -28,8 +32,23 @@ export class CandidatesComponent implements OnInit {
     )
   }
 
+  getEmail(candidate: Candidate): string{
+    let email = ""
+    for(let i = 0; i< candidate.candidateAttributes.length;i++){
+      if(candidate.candidateAttributes[i].attributeTypes.name === 'email') {
+        email = candidate.candidateAttributes[i].value;
+        break
+      }
+    }
+    return email
+  }
+
   ngOnInit() {
     this.getCandidates();
+  }
+
+  parseDate(date: Date): string{
+    return new Date(date).toLocaleString()
   }
 
   Search(){
@@ -41,4 +60,25 @@ export class CandidatesComponent implements OnInit {
       })
     }
   }
+
+  getCandidateFIO(candidate: Candidate): string{
+    let firstname = ""
+    let lastname = ""
+    for(let i = 0; i < candidate.candidateAttributes.length; i++){
+      if(candidate.candidateAttributes[i].attributeTypes.name.toLocaleLowerCase().match("firstname".toLocaleLowerCase()))
+        firstname = candidate.candidateAttributes[i].value
+      if(candidate.candidateAttributes[i].attributeTypes.name.toLocaleLowerCase().match("lastname".toLocaleLowerCase()))
+        lastname = candidate.candidateAttributes[i].value
+    }
+    return firstname + " " + lastname;
+  }
+
+  getCandidateKeywords(candidate: Candidate): string[]{
+    let keywords: string[] = []
+    for(let i = 0; i < candidate.keywords.length;i++){
+      keywords.push(candidate.keywords[i].id)
+    }
+    return keywords;
+  }
+
 }
